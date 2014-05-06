1e972228e6bfd3edae3d371f03acf63296f75601
mariano-williner <mariano_williner@hotmail.com>
2014-05-06 00:27:09 -0300

    Se agregó pregunta faltante en la spec.

--- Escritorio/Grupo9/Installer.sh
+++ Escritorio/Grupo9/Installer.sh
@@ -21,26 +21,28 @@ SIZE=""
 FREESPACE=`df -m "$GRUPO" | tail -1 | awk '{print $4}'`
 LOGBIN=./Logging.sh
 MOVEBIN=./Mover.sh
+CONFIRMINSTALL="false"
 
 
 #Messages
 INFO_DIRNAME_CANNOT_BE_EMPTY="El nombre del directorio no puede ser vacío"
 INFO_SIZE_MUST_BE_NUMERIC="El tamaño debe ser numérico"
 INFO_INSTALLER_EXECUTION_STARTED="Inicio de Ejecución de Installer"
-INFO_COPYRIGHT="TP SO7508 Primer Cuatrimestre 2014. Tema C Copyright © Grupo 09"
+INFO_COPYRIGHT="TP SO7508 Primer Cuatrimestre 2014. Tema C Copyright © Grupo 09\n"
 TITLE_MISSING_COMPONENTS="Componentes faltantes:"
 INFO_INSTALLATION_STATUS_READY="Estado de la instalación: LISTA"
 INFO_INSTALLATION_STATUS_INCOMPLETE="Estado de la instalación: INCOMPLETA"
 INFO_INSTALLATION_STATUS_COMPLETE="Estado de la instalación: COMPLETA"
 INFO_INSTALLATION_CANCELED="Proceso de Instalación Cancelado"
-QUESTION_COMPLETE_INSTALLATION="Desea completar la instalación? (Si-No):"
+QUESTION_COMPLETE_INSTALLATION="Desea completar la instalación? (Si-No): "
 INFO_INSTALLATION_CANCELED_BY_USER="Proceso de Instalación Cancelado por el usuario"
 INFO_ANSWER_MUST_BE_YES_OR_NO="Por favor responda Si o No."
 INFO_TERMS_AND_CONDITIONS="Al  instalar  TP  SO7508  Primer  Cuatrimestre  2014  UD.  expresa  aceptar  los\ntérminos  y  condiciones  del  \"ACUERDO  DE  LICENCIA  DE  SOFTWARE\"  incluido  en\neste paquete."
 QUESTION_ACCEPT_TERMS_AND_CONDITIONS="Acepta? Si - No: "
 INFO_TERMS_AND_CONDS_NOT_ACCEPTED="El usuario no aceptó los términos y condiciones. Instalación cancelada"
 INFO_PERL_VERSION="Perl version:\n`perl -v`"
-QUESTION_START_INSTALLATION="Iniciando Instalación. Esta Ud. seguro? (Si-No)"
+QUESTION_CONFIRM_INSTALLATION="Condirma Instalación? (Si-No): "
+QUESTION_START_INSTALLATION="Iniciando Instalación. Esta Ud. seguro? (Si-No): "
 TITLE_CREATING_DIRECTORY_STRUCTURE="Creando Estructuras de directorio. . . ."
 TITLE_INSTALLING_MASTER_FILES_AND_TABLES="Instalando Archivos Maestros y Tablas"
 TITLE_INSTALLING_PROGRAMS_AND_FUNCTIONS="Instalando Programas y Funciones"
@@ -363,7 +365,7 @@ if [ ! ${#INSTALLED[@]} -eq 0 ]
 
 				if [ `isMissing "$BINDIR"` == "true" ]
 					then
-						log "Directorio  Ejecutables: $BINDIR" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+						log "Directorio  Ejecutables: $BINDIR\n" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
 				fi
 
 				if [ `isMissing "$MAEDIR"` == "true" ]
@@ -458,120 +460,165 @@ if [ $PERLVER -lt $MINPERLVER ]
 		log "$INFO_COPYRIGHT\n$INFO_PERL_VERSION" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
 fi
 
-#HAS TO BE DONE THIS WAY BECAUSE SHELL SCRIPTING SUCKS
-if [ `isMissing "$BINDIR"` == "true" ]
-	then
-		askForDirectoryName "Defina el directorio de instalación de los ejecutables ($BINDIR): "
-		replaceInMissing "$BINDIR" "$DIRNAME"
-		BINDIR=$DIRNAME
-		DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
-		log "Defina el directorio de instalación de los ejecutables ($BINDIR): $BINDIR" "$GRUPO/$CONFDIR/installer" "INFO"
-fi
+while [ "$CONFIRMINSTALL" == "false" ];
+do
+	unset DIRECTORYNAMESTAKEN
 
-if [ `isMissing "$MAEDIR"` == "true" ]
-	then
-		askForDirectoryName "Defina directorio para maestros y tablas ($MAEDIR): "
-		replaceInMissing "$MAEDIR" "$DIRNAME"
-		MAEDIR=$DIRNAME
-		DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
-		log "Defina directorio para maestros y tablas ($MAEDIR): $MAEDIR" "$GRUPO/$CONFDIR/installer" "INFO"
-fi
+	#HAS TO BE DONE THIS WAY BECAUSE SHELL SCRIPTING SUCKS
+	if [ `isMissing "$BINDIR"` == "true" ]
+		then
+			askForDirectoryName "Defina el directorio de instalación de los ejecutables ($BINDIR): "
+			replaceInMissing "$BINDIR" "$DIRNAME"
+			BINDIR=$DIRNAME
+			DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
+			log "Defina el directorio de instalación de los ejecutables ($BINDIR): $BINDIR" "$GRUPO/$CONFDIR/installer" "INFO"
+	fi
+	
+	if [ `isMissing "$MAEDIR"` == "true" ]
+		then
+			askForDirectoryName "Defina directorio para maestros y tablas ($MAEDIR): "
+			replaceInMissing "$MAEDIR" "$DIRNAME"
+			MAEDIR=$DIRNAME
+			DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
+			log "Defina directorio para maestros y tablas ($MAEDIR): $MAEDIR" "$GRUPO/$CONFDIR/installer" "INFO"
+	fi
+	
+	if [ `isMissing "$NOVEDIR"` == "true" ]
+		then
+			askForDirectoryName "Defina el Directorio de arribo de novedades ($NOVEDIR): "
+			replaceInMissing "$NOVEDIR" "$DIRNAME"
+			NOVEDIR=$DIRNAME
+			DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
+			log "Defina el Directorio de arribo de novedades ($NOVEDIR): $NOVEDIR" "$GRUPO/$CONFDIR/installer" "INFO"
+	
+			askForSize "Defina espacio mínimo libre para el arribo de novedades en Mbytes ($DATASIZE): "
+			DATASIZE=$SIZE
+			log "Defina espacio mínimo libre para el arribo de novedades en Mbytes ($DATASIZE): $DATASIZE" "$GRUPO/$CONFDIR/installer" "INFO"
+			while [ true ]
+			do
+				if [ $FREESPACE -lt $DATASIZE ]
+					then
+						log "Insuficiente espacio en disco.\nEspacio disponible: $FREESPACE Mb.\nEspacio requerido: $DATASIZE Mb.\nCancele la instalación o inténtelo nuevamente." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+						askForSize "Defina espacio mínimo libre para el arribo de novedades en Mbytes ($DATASIZE): "
+						DATASIZE=$SIZE
+						log "Defina espacio mínimo libre para el arribo de novedades en Mbytes ($DATASIZE): $DATASIZE" "$GRUPO/$CONFDIR/installer" "INFO"
+					else
+						break
+				fi
+			done
+	fi
+	
+	if [ `isMissing "$ACEPDIR"` == "true" ]
+		then
+			askForDirectoryName "Defina el directorio de grabación de las Novedades aceptadas ($ACEPDIR): "
+			replaceInMissing "$ACEPDIR" "$DIRNAME"
+			ACEPDIR=$DIRNAME
+			DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
+			log "Defina el directorio de grabación de las Novedades aceptadas ($ACEPDIR): $ACEPDIR" "$GRUPO/$CONFDIR/installer" "INFO"
+	fi
+	
+	if [ `isMissing "$INFODIR"` == "true" ]
+		then
+			askForDirectoryName "Defina el directorio de grabación de los informes de salida ($INFODIR): "
+			replaceInMissing "$INFODIR" "$DIRNAME"
+			INFODIR=$DIRNAME
+			DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
+			log "Defina el directorio de grabación de los informes de salida ($INFODIR): $INFODIR" "$GRUPO/$CONFDIR/installer" "INFO"
+	fi
+	
+	if [ `isMissing "$RECHDIR"` == "true" ]
+		then
+			askForDirectoryName "Defina el directorio de grabación de Archivos rechazados ($RECHDIR): "
+			replaceInMissing "$RECHDIR" "$DIRNAME"
+			RECHDIR=$DIRNAME
+			DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
+			log "Defina el directorio de grabación de Archivos rechazados ($RECHDIR): $RECHDIR" "$GRUPO/$CONFDIR/installer" "INFO"
+	fi
+	
+	if [ `isMissing "$LOGDIR"` == "true" ]
+		then
+			askForDirectoryName "Defina el directorio de logs ($LOGDIR): "
+			replaceInMissing "$LOGDIR" "$DIRNAME"
+			LOGDIR=$DIRNAME
+			DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
+			log "Defina el directorio de logs ($LOGDIR): $LOGDIR" "$GRUPO/$CONFDIR/installer" "INFO"
+	
+			LOGEXT=`askUser "Ingrese la extensión para los archivos de log: (.$LOGEXT): "`
+			log "Ingrese la extensión para los archivos de log: (.$LOGEXT): $LOGEXT" "$GRUPO/$CONFDIR/installer" "INFO"
+	
+			askForSize "Defina el tamaño máximo para los archivos $LOGEXT en Kbytes ($LOGSIZE): "
+			LOGSIZE=$SIZE
+			log "Defina el tamaño máximo para los archivos $LOGEXT en Kbytes ($LOGSIZE): $LOGSIZE" "$GRUPO/$CONFDIR/installer" "INFO"
+			while [ true ]
+			do
+				if [ $LOGSIZE -lt $MINLOGSIZEINKB -o $LOGSIZE -gt $MAXLOGSIZEINKB ]
+					then
+						log "El tamaño máximo para los archivo $LOGEXT debe estar entre $MINLOGSIZEINKB  y $MAXLOGSIZEINKB. Por favor ingrese un valor en dicho rango" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+						askForSize "Defina el tamaño máximo para los archivos $LOGEXT en Kbytes ($LOGSIZE): "
+						LOGSIZE=$SIZE
+						log "Defina el tamaño máximo para los archivos $LOGEXT en Kbytes ($LOGSIZE): $LOGSIZE" "$GRUPO/$CONFDIR/installer" "INFO"
+					else
+						break
+				fi
+			done
+	fi
 
-if [ `isMissing "$NOVEDIR"` == "true" ]
-	then
-		askForDirectoryName "Defina el Directorio de arribo de novedades ($NOVEDIR): "
-		replaceInMissing "$NOVEDIR" "$DIRNAME"
-		NOVEDIR=$DIRNAME
-		DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
-		log "Defina el Directorio de arribo de novedades ($NOVEDIR): $NOVEDIR" "$GRUPO/$CONFDIR/installer" "INFO"
+	log "$INFO_COPYRIGHT" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+	log "Direct. de Configuración: $CONFDIR.\n" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
 
-		askForSize "Defina espacio mínimo libre para el arribo de novedades en Mbytes ($DATASIZE): "
-		DATASIZE=$SIZE
-		log "Defina espacio mínimo libre para el arribo de novedades en Mbytes ($DATASIZE): $DATASIZE" "$GRUPO/$CONFDIR/installer" "INFO"
-		while [ true ]
-		do
-			if [ $FREESPACE -lt $DATASIZE ]
-				then
-					log "Insuficiente espacio en disco.\nEspacio disponible: $FREESPACE Mb.\nEspacio requerido: $DATASIZE Mb.\nCancele la instalación o inténtelo nuevamente." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-					askForSize "Defina espacio mínimo libre para el arribo de novedades en Mbytes ($DATASIZE): "
-					DATASIZE=$SIZE
-					log "Defina espacio mínimo libre para el arribo de novedades en Mbytes ($DATASIZE): $DATASIZE" "$GRUPO/$CONFDIR/installer" "INFO"
-				else
-					break
-			fi
-		done
-fi
+	if [ `isMissing "$BINDIR"` == "true" ]
+		then
+			log "Directorio Ejecutables: $BINDIR.\n" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+	fi
 
-if [ `isMissing "$ACEPDIR"` == "true" ]
-	then
-		askForDirectoryName "Defina el directorio de grabación de las Novedades aceptadas ($ACEPDIR): "
-		replaceInMissing "$ACEPDIR" "$DIRNAME"
-		ACEPDIR=$DIRNAME
-		DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
-		log "Defina el directorio de grabación de las Novedades aceptadas ($ACEPDIR): $ACEPDIR" "$GRUPO/$CONFDIR/installer" "INFO"
-fi
+	if [ `isMissing "$MAEDIR"` == "true" ]
+		then
+			log "Direct Maestros y Tablas: $MAEDIR.\n" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+	fi
 
-if [ `isMissing "$INFODIR"` == "true" ]
-	then
-		askForDirectoryName "Defina el directorio de grabación de los informes de salida ($INFODIR): "
-		replaceInMissing "$INFODIR" "$DIRNAME"
-		INFODIR=$DIRNAME
-		DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
-		log "Defina el directorio de grabación de los informes de salida ($INFODIR): $INFODIR" "$GRUPO/$CONFDIR/installer" "INFO"
-fi
+	if [ `isMissing "$NOVEDIR"` == "true" ]
+		then
+			log "Directorio de Novedades: $NOVEDIR.\n" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+			log "Espacio mínimo libre para arribos: $DATASIZE Mb." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+	fi
 
-if [ `isMissing "$RECHDIR"` == "true" ]
-	then
-		askForDirectoryName "Defina el directorio de grabación de Archivos rechazados ($RECHDIR): "
-		replaceInMissing "$RECHDIR" "$DIRNAME"
-		RECHDIR=$DIRNAME
-		DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
-		log "Defina el directorio de grabación de Archivos rechazados ($RECHDIR): $RECHDIR" "$GRUPO/$CONFDIR/installer" "INFO"
-fi
+	if [ `isMissing "$ACEPDIR"` == "true" ]
+		then
+			log "Dir. Novedades Aceptadas: $ACEPDIR.\n" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+	fi
 
-if [ `isMissing "$LOGDIR"` == "true" ]
-	then
-		askForDirectoryName "Defina el directorio de logs ($LOGDIR): "
-		replaceInMissing "$LOGDIR" "$DIRNAME"
-		LOGDIR=$DIRNAME
-		DIRECTORYNAMESTAKEN=("${DIRECTORYNAMESTAKEN[@]}" "$DIRNAME")
-		log "Defina el directorio de logs ($LOGDIR): $LOGDIR" "$GRUPO/$CONFDIR/installer" "INFO"
+	if [ `isMissing "$INFODIR"` == "true" ]
+		then
+			log "Dir. Informes de Salida: $INFODIR.\n" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+	fi
 
-		LOGEXT=`askUser "Ingrese la extensión para los archivos de log: (.$LOGEXT): "`
-		log "Ingrese la extensión para los archivos de log: (.$LOGEXT): $LOGEXT" "$GRUPO/$CONFDIR/installer" "INFO"
+	if [ `isMissing "$RECHDIR"` == "true" ]
+		then
+			log "Dir. Archivos Rechazados: $RECHDIR.\n" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+	fi
 
-		askForSize "Defina el tamaño máximo para los archivos $LOGEXT en Kbytes ($LOGSIZE): "
-		LOGSIZE=$SIZE
-		log "Defina el tamaño máximo para los archivos $LOGEXT en Kbytes ($LOGSIZE): $LOGSIZE" "$GRUPO/$CONFDIR/installer" "INFO"
-		while [ true ]
-		do
-			if [ $LOGSIZE -lt $MINLOGSIZEINKB -o $LOGSIZE -gt $MAXLOGSIZEINKB ]
-				then
-					log "El tamaño máximo para los archivo $LOGEXT debe estar entre $MINLOGSIZEINKB  y $MAXLOGSIZEINKB. Por favor ingrese un valor en dicho rango" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-					askForSize "Defina el tamaño máximo para los archivos $LOGEXT en Kbytes ($LOGSIZE): "
-					LOGSIZE=$SIZE
-					log "Defina el tamaño máximo para los archivos $LOGEXT en Kbytes ($LOGSIZE): $LOGSIZE" "$GRUPO/$CONFDIR/installer" "INFO"
-				else
-					break
-			fi
-		done
-fi
+	if [ `isMissing "$LOGDIR"` == "true" ]
+		then
+			log "Dir. de Logs de Comandos: $LOGDIR/<comando>.$LOGEXT.\n" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+			log "Tamaño máximo para los archivos de log del sistema: $LOGSIZE Kb.\n" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+	fi
+
+	log "Estado de la instalación: LISTA.\n" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
+
+	while [ true ];
+	do
+		case `askUser "$QUESTION_CONFIRM_INSTALLATION"` in
+			"Si" )	CONFIRMINSTALL="true"
+					break;;
+			"No" )	log "El usuario decidió reingresar los nombres de directorios" "$GRUPO/$CONFDIR/installer" "INFO"
+					break;;
+			* )		echo "$INFO_ANSWER_MUST_BE_YES_OR_NO";;
+		esac
+	done
+done
 
 #UNCOMMENT FOR FINAL RELEASE
 #clear
-
-log "$INFO_COPYRIGHT" "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-log "Direct. de Configuración: $CONFDIR." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-log "Directorio Ejecutables: $BINDIR." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-log "Direct Maestros y Tablas: $MAEDIR." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-log "Directorio de Novedades: $NOVEDIR." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-log "Espacio mínimo libre para arribos: $DATASIZE Mb." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-log "Dir. Novedades Aceptadas: $ACEPDIR." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-log "Dir. Informes de Salida: $INFODIR." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-log "Dir. Archivos Rechazados: $RECHDIR." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-log "Dir. de Logs de Comandos: $LOGDIR/<comando>.$LOGEXT." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-log "Tamaño máximo para los archivos de log del sistema: $LOGSIZE Kb." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
-log "Estado de la instalación: LISTA." "$GRUPO/$CONFDIR/installer" "INFO" doEcho
 
 while [ true ];
 do
